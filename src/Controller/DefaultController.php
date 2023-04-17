7b64b4bb01f4fb825328cb6ab3e0b7a96e3b14b6
Barsbold B <barsa.bazar@gmail.com>
2023-04-17 18:05:29 +0200

    Spa v1

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,4 +13,10 @@ class DefaultController extends AbstractController
     {
         return $this->render('default/show_home.html.twig');
     }
+
+    #[Route('/spa', name: 'show_spa', methods: ['GET'])]
+    public function showSpa(): Response
+    {
+        return $this->render('default/show_spa.html.twig');
+    }
 }
