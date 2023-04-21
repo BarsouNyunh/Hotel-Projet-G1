9e02651ee32c80384ef4d87161050acca9a0d2f3
Barsbold B <barsa.bazar@gmail.com>
2023-04-21 12:33:32 +0200

    Update DefaultController.php V2

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Slider;
 use App\Repository\SliderRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -64,17 +63,5 @@ class DefaultController extends AbstractController
     {
         return $this->render('contact/nous.html.twig');
     } // end showMap()
-
-
-    // #[Route('/{cat_alias}/{art_alias}_{id}.html', name: 'show_one_chambre', methods: ['GET'])]
-    // public function showOneChambre(Chambre $chambre, AvisRepository $avisRepository): Response
-    // {
-    //     $items = $avisRepository->findBy(['deletedAt' => null, 'chambre' => $chambre->getId()]);
-
-    //     return $this->render('default/show_chambre.html.twig', [
-    //         'chambre' => $chambre,
-    //         'items' => $items
-    //     ]);
-    // }
 
 }
