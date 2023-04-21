88453e0a0deaff73709f8bf6ddba2aec28c9d7a2
Mariama Tunkara <119751878+mariama2244@users.noreply.github.com>
2023-04-21 14:21:20 +0200

    controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Slider;
 use App\Repository\SliderRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
